6ea15b380e329a54160b32badc5f1ea41e11f50d
Evin Odisho <edodisho@ucsc.edu>
2021-12-21 19:31:37 -0800

    Got this working, wohooo

--- fahrenheitToCel/fahrenheitToCel/main.c
+++ fahrenheitToCel/fahrenheitToCel/main.c
@@ -9,13 +9,9 @@
 
 int main(void) {
     double far, cel;
-    double conv = -32 * (5/9);
     printf("Enter farhenheit value: ");
     scanf ("%lf", &far);
-    //debug
-    printf("far is %lf\n", far);
-    cel = ((far - 32) * (5/9));
-    //cel = far + conv;
+    cel = (far - 32) * 5/9;
     printf("%lf degrees farhenheit is equal to %lf degrees celsius.\n", far, cel);
     return 0;
 }

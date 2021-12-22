4b28592ada4c938f1afdeb243e7419372a57fbe4
Evin Odisho <edodisho@ucsc.edu>
2021-12-21 20:02:40 -0800

    Text file for coursera sumbission that has correct code for Dr. P's mistake in week 1.

---
+++ Fix Mistake (Week 1)/Fix Mistake (Week 1)/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Fix Mistake (Week 1)
+//
+//  Created by Evin Odisho on 12/21/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

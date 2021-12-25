f1b576a7ec387029284c7852f33a9037039f535e
Evin Odisho <edodisho@ucsc.edu>
2021-12-24 17:42:09 -0800

    Made this program off coursera, woohoooo.

---
+++ courseraSpeedTicket/courseraSpeedTicket/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  courseraSpeedTicket
+//
+//  Created by Evin Odisho on 12/24/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

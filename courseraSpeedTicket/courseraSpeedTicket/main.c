fee667917a8040d6a1d265a43f0106fd7babdcd0
Evin Odisho <edodisho@ucsc.edu>
2021-12-24 17:42:41 -0800

    Made this program off coursera, woohoooo.

--- courseraSpeedTicket/courseraSpeedTicket/main.c
+++ courseraSpeedTicket/courseraSpeedTicket/main.c
@@ -9,6 +9,20 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    // Takes in speed, and determines if ticket is issued or not
+    // Validates user input
+    double speed;
+    printf("Please enter speed (in MPH): ");
+    scanf("%lf", &speed);
+    // Input check
+    while (speed < 1 || speed > 100){
+        printf("Enter valid speed (between 1 and 100: ");
+        scanf("%lf", &speed);
+    }
+    if (speed >= 65.00)
+        printf("You get a ticket!\n");
+    else
+        printf("No ticket for you!\n");
     return 0;
 }
+

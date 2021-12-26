dcb07e3cf098c3dc090a7d62dc76942fc99806b0
Evin Odisho <edodisho@ucsc.edu>
2021-12-25 18:15:38 -0800

    Checking for differences between C and C++ For loops (very similar)

---
+++ forLoop/forLoop/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  forLoop
+//
+//  Created by Evin Odisho on 12/25/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    for (int i = 5; i > 0; --i){
+        printf("%d\n", i);
+    }
+    return 0;
+}

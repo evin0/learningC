8925ee1fd70b064ca9ece59e02044268c23d10ef
evin0 <edodisho@ucsc.edu>
2022-01-04 00:44:33 -0800

    Changed array size.

--- storageClass/storageClass.c
+++ storageClass/storageClass.c
@@ -3,7 +3,7 @@
 // External/Global Variable
 extern char extStr[] = "Hey there!\n";
 
-void name(char nameIn[100]){
+void name(char nameIn[50]){
     printf("What's your name? ");
     // Can't just do regular scanf %s when taking in string input
     scanf("%[^\n]%*c", nameIn);
@@ -14,7 +14,7 @@ void name(char nameIn[100]){
 
 int main(){
     printf("%s", extStr);
-    char nameInput[100];
+    char nameInput[50];
     name(nameInput);
     char myChar;
     printf("Enter a single character: ");

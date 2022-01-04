bd7c4a6478f2f1e0a8058ce1953554beea42a4c8
evin0 <edodisho@ucsc.edu>
2022-01-04 00:34:50 -0800

    Made a storageClass file to learn more about external variables, and multiple functions within a sing;e .c file.

---
+++ storageClass/storageClass.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// External/Global Variable
+extern char extStr[] = "Hey there!\n";
+
+void name(char nameIn[100]){
+    printf("What's your name? ");
+    // Can't just do regular scanf %s when taking in string input
+    scanf("%[^\n]%*c", nameIn);
+    printf("Nice to meet you %s!\n", nameIn);
+    return;
+}
+
+
+int main(){
+    printf("%s", extStr);
+    char nameInput[100];
+    name(nameInput);
+    char myChar;
+    printf("Enter a single character: ");
+    scanf("%c", &myChar);
+    printf("The character is: %c\n", myChar);
+    return 0;
+}
